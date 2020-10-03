282b30c9011d9ff082c7ecc7fe9b1582482c19fd
Matt Wagaman <averaqedev@gmail.com>
2020-10-02 18:26:59 -0700

    finished navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,100 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CSSTransition } from 'react-transition-group';
 
 function App() {
-  const [count, setCount] = useState(0);
-  setTimeout(() => setCount(count + 1), 1000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <Navbar>
+      <NavItem icon="🏠" />
+      <NavItem icon="🔥" />
+      <NavItem icon="⚙️" />
+
+      <NavItem icon="🔽">
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
+    </Navbar>
   );
+}
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  )
+}
+
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+
+      {open && props.children}
+    </li>
+  );
+}
+
+function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  function DropdownItem(props) {
+    return (
+      <a 
+        href="#" 
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    )
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }}>
+      <CSSTransition
+      onEnter={calcHeight}
+        in={activeMenu === 'main'}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+      >
+        <div className='menu'>
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon="⚙️"
+            rightIcon="→"
+            goToMenu="settings"
+          >
+            Settings
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "settings"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon="◀️">
+            <h2>Go back</h2>
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  )
 }
 
 export default App;
